6f55ef22672255400f9ef43b9d8ad2a9d13153e9
Ana Julia <anajuliadefendi@gmail.com>
2025-12-01 21:05:55 -0300

    CRUD ator_do_filme

--- src/server.js
+++ src/server.js
@@ -9,11 +9,12 @@ import { salaRoutes } from './modules/sala/sala.routes.js';
 import { pedidoRoutes } from './modules/pedido/pedido.routes.js';
 import { filmeRoutes } from './modules/filme/filme.routes.js';
 import { assentoRoutes } from './modules/assento/assento.routes.js';
+import { atorDoFilmeRoutes } from './modules/ator_do_filme/ator_do_filme.routes.js';
 
 const app = Fastify();
 
 await app.register(cors, {
-    origin: '*', 
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 });
 
@@ -26,6 +27,7 @@ app.register(salaRoutes)
 app.register(pedidoRoutes)
 app.register(filmeRoutes)
 app.register(assentoRoutes)
+app.register(atorDoFilmeRoutes)
 
 app.listen({ port: 3333 }, () => {
     console.log('Servidor rodando na porta 3333');
